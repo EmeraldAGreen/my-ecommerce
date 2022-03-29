f2c5de27663419d88df2258f0aea4662fc3239a3
Emerge <egreengirl1@gmail.com>
2022-03-28 22:35:57 -0600

    updated TODOs in Models directory; i.e. created columns and model associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete:'CASCADE'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  // If you want to set several target instances, but with different attributes you have to set the attributes on the instance, using a property with the name of the through model:
+  through: ProductTag
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
 module.exports = {
   Product,
